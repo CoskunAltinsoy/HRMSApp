95e8cbaae2e81b9199c818846493fef2f608e28d
CoskunAltinsoy <80579668+CoskunAltinsoy@users.noreply.github.com>
2021-05-22 17:01:47 +0300

    Hrms

---
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/JobPositionsControllers.java
@@ -0,0 +1,31 @@
+package kodlamaio.hrms.api.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.hrms.business.abstracts.JobPositionService;
+import kodlamaio.hrms.entities.concretes.JobPosition;
+
+@RestController
+@RequestMapping("/api/jobpositions")
+public class JobPositionsControllers {
+	
+	private JobPositionService jobPositionService;
+    
+	@Autowired
+	public JobPositionsControllers(JobPositionService jobPositionService) {
+		super();
+		this.jobPositionService = jobPositionService;
+	}
+
+	
+	@GetMapping("/getall")
+	public List<JobPosition> getAll(){
+		return jobPositionService.getAll();
+	}
+
+}
